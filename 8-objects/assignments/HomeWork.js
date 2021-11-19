0ee0955b32b11ad8a1cd18f83e7ab81a699d5042
JOJOKE21 <kingjordan2122@gmail.com>
2021-11-19 08:03:46 -0500

    HW

--- 8-objects/assignments/HomeWork.js
+++ 8-objects/assignments/HomeWork.js
@@ -19,21 +19,70 @@
 // // 3.   Using bracket-notation Log to the console 3 properties from the previous object
 // car['color'] = 'purple'
 // car['engine'] = 'V8'
-// car['config'] = 'sedan'
-// // console.log(car)
+// // car['config'] = 'sedan'
+// // // console.log(car)
 
-// // // 4.   Try and log a property that doesn't already exist - what output do we get?
-// // 'we would get a not defined'
-// // // 5.   Add a new key-value pair to the vehicle. 
-// // car.mpg = '12'
+// // // // 4.   Try and log a property that doesn't already exist - what output do we get?
+// // // 'we would get a not defined'
+// // // // 5.   Add a new key-value pair to the vehicle. 
+// // // car.mpg = '12'
+// // // console.log(car)
+// // // // 6.   Using bracket-notation update a property on the vehicle. 
+// // // car['color'] = 'red'
+// // // console.log(car)
+// // // // 7.   Using dot-notation update a property on the vehicle. 
+// // // car.mpg = '21'
+// // // console.log(car)
+// // // 8.   Create a method for turning your vehicle on
+
+// // const car  = {
+// //     color: 'black',
+// //     model: '2022 dodge charger',
+// //     engine: 'V8',
+// //     config: 'sedan',
+// //     horsepower: '717-797',
+// //     price: '$72,350',
+// //     start: function(){
+// //     console.log("Vroom Vroom");
+// // } 
+// // }
 // // console.log(car)
-// // // 6.   Using bracket-notation update a property on the vehicle. 
-// // car['color'] = 'red'
+// // function startCar(){
+// //     console.log("Vroom Vroom!");
+// // }
+
+// // car.start = startCar;
+
+// // car.start();
+
+
+// // // 9.   Create a method for turning your vehicle off 
+// // const car  = {
+// //     color: 'black',
+// //     model: '2022 dodge charger',
+// //     engine: 'V8',
+// //     config: 'sedan',
+// //     horsepower: '717-797',
+// //     price: '$72,350',
+// //     off: function(){
+// //     console.log('Power down');
+// // } 
+// // }
 // // console.log(car)
-// // // 7.   Using dot-notation update a property on the vehicle. 
-// // car.mpg = '21'
-// // console.log(car)
-// // 8.   Create a method for turning your vehicle on
+// // function turnOfCar(){
+// //     console.log('Power dowm')
+// // }
+// // car.off = turnOfCar;
+// // car.off()
+
+
+
+// //10.   
+// //      a. Check if your vehicle is on (it should be off)
+// //      b. Start your vehicle
+// //      c. Check if your vehicle is on (it should be on)
+// //      d. Stop your vehicle
+// //      e. Check if your vehicle is on (it should be on)
 
 // const car  = {
 //     color: 'black',
@@ -54,52 +103,29 @@
 // car.start = startCar;
 
 // car.start();
-
-
-// // 9.   Create a method for turning your vehicle off 
-// const car  = {
-//     color: 'black',
-//     model: '2022 dodge charger',
-//     engine: 'V8',
-//     config: 'sedan',
-//     horsepower: '717-797',
-//     price: '$72,350',
-//     off: function(){
-//     console.log('Power down');
-// } 
+// const name = "Johnny"
+// function greet(name){ 
+//      return `Hello, ${name}!`;
 // }
-// console.log(car)
-// function turnOfCar(){
-//     console.log('Power dowm')
-// }
-// car.off = turnOfCar;
-// car.off()
+// if(name === "Johnny"){
+//       return "Hello, my love!";
+//   }
+// greet()
 
 
+function DNAtoRNA(dna) {
+for(let i = 0; i < dna.length; i++){
+    const currentLetter = dan[i]
+    const letters = ['G', 'C', 'A', 'T']
+   let rna = []
+    if(dna[i] === 'T'){
+    rna = rna = 'U'
+    }else { 
+    rna = rna + dna[i]
+}
 
-//10.   
-//      a. Check if your vehicle is on (it should be off)
-//      b. Start your vehicle
-//      c. Check if your vehicle is on (it should be on)
-//      d. Stop your vehicle
-//      e. Check if your vehicle is on (it should be on)
-
-const car  = {
-    color: 'black',
-    model: '2022 dodge charger',
-    engine: 'V8',
-    config: 'sedan',
-    horsepower: '717-797',
-    price: '$72,350',
-    start: function(){
-    console.log("Vroom Vroom");
 } 
-}
-console.log(car)
-function startCar(){
-    console.log("Vroom Vroom!");
-}
+return Rna
+  }
 
-car.start = startCar;
-
-car.start();
+return 
