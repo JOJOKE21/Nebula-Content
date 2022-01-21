967d15fcd98c20231d0f4d1712712877f8d84d1c
JOJOKE21 <kingjordan2122@gmail.com>
2022-01-21 10:23:44 -0500

    L

--- API/assaingmengt/api-practice/src/componets/Cars/cars.js
+++ API/assaingmengt/api-practice/src/componets/Cars/cars.js
@@ -2,12 +2,12 @@ import React from "react";
 import "./cars.css"
 
 export default function Cars(props){
-    const {Name, Year, TopSpped, HorsePower, TwoDoor, Price} = props.cars
+    const {Name, Year, TopSpeed, HorsePower, TwoDoor, Price} = props.cars
     return(
         <div id="Cars">
             <h1>{Name}</h1>
             <p>{Year}</p>
-            <p>{TopSpped}</p>
+            <p>{TopSpeed}</p>
             <p>{HorsePower}</p>
             <p>{TwoDoor}</p>
             <p>{Price}</p>
